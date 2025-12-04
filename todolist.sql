c3c1fd4bf8e60773bafc0f7cab107c480a9fb258
maulidacitraa <maulida.ellis@gmail.com>
2025-12-04 21:55:21 +0800

    maulida (update db)

--- todolist.sql
+++ todolist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2025 at 04:22 AM
+-- Generation Time: Dec 04, 2025 at 02:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,14 +30,43 @@ SET time_zone = "+00:00";
 CREATE TABLE `todo` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `type` enum('TASK','ACTIVITY','EVENT') NOT NULL,
   `title` varchar(200) NOT NULL,
   `description` text DEFAULT NULL,
-  `date` date NOT NULL,
+  `date` datetime NOT NULL,
   `priority` enum('LOW','MEDIUM','HIGH') NOT NULL,
-  `deadline` date DEFAULT NULL,
-  `location` varchar(200) DEFAULT NULL,
-  `eventTime` time DEFAULT NULL
+  `type` enum('TASK','ACTIVITY','EVENT') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todo_activity`
+--
+
+CREATE TABLE `todo_activity` (
+  `id` int(11) NOT NULL,
+  `location` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todo_event`
+--
+
+CREATE TABLE `todo_event` (
+  `id` int(11) NOT NULL,
+  `duration_hours` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todo_task`
+--
+
+CREATE TABLE `todo_task` (
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -62,6 +91,24 @@ CREATE TABLE `users` (
 ALTER TABLE `todo`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `todo_activity`
+--
+ALTER TABLE `todo_activity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `todo_event`
+--
+ALTER TABLE `todo_event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `todo_task`
+--
+ALTER TABLE `todo_task`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -95,6 +142,24 @@ ALTER TABLE `users`
 --
 ALTER TABLE `todo`
   ADD CONSTRAINT `todo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `todo_activity`
+--
+ALTER TABLE `todo_activity`
+  ADD CONSTRAINT `todo_activity_ibfk_1` FOREIGN KEY (`id`) REFERENCES `todo` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `todo_event`
+--
+ALTER TABLE `todo_event`
+  ADD CONSTRAINT `todo_event_ibfk_1` FOREIGN KEY (`id`) REFERENCES `todo` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `todo_task`
+--
+ALTER TABLE `todo_task`
+  ADD CONSTRAINT `todo_task_ibfk_1` FOREIGN KEY (`id`) REFERENCES `todo` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
